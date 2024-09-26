2f6263bd2a25b217d7f0a92bb3cfec5a36cc0f69
qnity001 <aishwaryam.aarya@gmail.com>
2024-09-26 18:08:38 +0530

    Changee background color upon hover

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeBackground(color) {
+    document.body.style.backgroundColor = color;
+    opacity = 0.9;
+}
+function resetBackground() {
+    document.body.style.backgroundColor = '#E3C8A8';
+}
